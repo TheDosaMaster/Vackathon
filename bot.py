20bb9f0d584d4be3d232568aacd4e58969bd6d6d
TheDosaMaster <tarun.devarajan@gmail.com>
2026-07-11 10:34:01 -0700

    monitoring messages

--- bot.py
+++ bot.py
@@ -13,10 +13,18 @@ bot = commands.Bot(command_prefix="/", intents=intents)
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    print(f"[{message.channel}] {message.author}: {message.content}")
+    await bot.process_commands(message)
 
 @bot.command()
 async def ping(ctx):
     print("Pinged")
     await ctx.send("hello")
+
 
 bot.run(os.getenv("DISCORD_TOKEN"))
